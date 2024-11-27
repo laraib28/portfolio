34c829d68219e870f3939da37b880f19719f612b
laraib28 <laraibadnan297@gmail.com>
2024-11-27 17:19:14 +0500

    update code

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,33 +1,106 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image';
+import emailjs from 'emailjs-com';
 
-export default function Contact () {
-    return ( 
-   <div className=" bg-black text-white ">
-   <div className="text-center font-bold p-10 text-4xl">
+export default function Contact() {
+  const [formData, setFormData] = useState({ email: '', message: '' });
+
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // EmailJS configuration
+    const serviceID = 'laraibadnan297@gmail.com';
+    const templateID = 'laraibadnan297@gmail.com';
+    const userID = 'laraibadnan297@gmail.com';
+
+    emailjs
+      .send(serviceID, templateID, formData, userID)
+      .then(
+        (result) => {
+          alert('Message sent successfully!');
+          setFormData({ email: '', message: '' }); // Clear form
+        },
+        (error) => {
+          console.error(error.text);
+          alert('Failed to send the message. Please try again.');
+        }
+      );
+  };
+
+  return (
+    <div className="bg-black text-black">
+      <div className="text-center font-bold p-10 text-4xl">
         <h1>Contact</h1>
-        </div>
-        <div className="text-center px-4">
-        <p>I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>
-        </div>
-         <div className="flex items-left gap-40 m-8 ml-96 ">
-            <a href="https://github.com/laraib28 " target='-blank' rel="noopener noreferrer" > 
-            <Image src="/git-link.webp" alt="GitHub" width={50} height={50}/>
-            </a>
-            <a href="https://www.linkedin.com/in/laraib-adnan-744955252/" target='-blank' rel="noopener noreferrer">
-            <Image src="/linkdin.png"  alt="linkedin"  width={40} height={50} />
-            </a>
-            <div className=' flex justify-center items-start gap-5'>
-                <label htmlFor="email" className= "text-white block mb-2 font-medium">Your email</label>
-                <input type="email" id='emial' placeholder='abc@gmail.com'className=' border-orange-600 rounded-lg border-4 divide-x-2 ' /> 
-                <label htmlFor="Message" className= "text-white block mb-2 text-sm font-medium">Message</label>
-                <input type="Message" id='Message' placeholder='Message ' required className= " border-orange-600 rounded-lg border-4 divide-x-2" /> 
-            <div>
-                <button className="items-end bg-orange-600 rounded-lg  p-2">Message</button>
-            </div>
-            </div>
+      </div>
+      <div className="text-center px-4">
+        <p>
+          I'm currently looking for new opportunities, my inbox is always open.
+          Whether you have a question or just want to say hi, I'll try my best
+          to get back to you!
+        </p>
+      </div>
+      <div className="flex items-center justify-center gap-5 m-8">
+        <a
+          href="https://github.com/laraib28"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/git-link.webp" alt="GitHub" width={50} height={50} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/laraib-adnan-744955252/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/linkdin.png" alt="linkedin" width={40} height={50} />
+        </a>
+      </div>
 
-        </div>
+      <div className="flex flex-col items-center gap-4">
+        <form onSubmit={sendEmail} className="w-full max-w-sm">
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-white mb-2 font-medium">
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="abc@gmail.com"
+              className="border-orange-600 rounded-lg border-4 w-full p-2"
+              value={formData.email}
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="block text-white mb-2 font-medium"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              placeholder="Message"
+              className="border-orange-600 rounded-lg border-4 w-full p-2"
+              value={formData.message}
+              onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+              required
+            />
+          </div>
+
+          <div className="text-center">
+            <button
+              type="submit"
+              className="bg-orange-600 rounded-lg text-white px-4 py-2"
+            >
+              Send Message
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-)}
+  );
+}
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,19 +1,18 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import Typewriter from 'typewriter-effect';
 
 export default function Hero() {
     return (
         <main className="bg-black text-white">
-            <div className="flex flex-col md:flex-row items-center px-6 mx-0 space-y-0  ">
+            <div className="flex flex-col md:flex-row items-center px-6 mx-auto space-y-10 md:space-y-0 md:space-x-10 max-w-screen-lg">
                 {/* Text Section */}
-                <div className="flex-1 mb-32 md:w-1/2 md:pr-10 sm:pr-10">
-                    <h1 className="py-10 max-w-md text-4xl font-bold text-left text-orange-400">
+                <div className="flex-1 mb-32 md:w-1/2 md:pr-40 sm:px-6">
+                    <h1 className="py-10 max-w-md text-3xl sm:text-4xl font-bold text-left text-orange-400">
                         Hello! I Am
                     </h1>
-                    <div className="text-4xl font-bold text-orange-200 mb-4">
-                        <Typewriter 
+                    <div className="text-2xl sm:text-4xl font-bold text-orange-200 mb-4">
+                        <Typewriter
                             options={{
                                 strings: ['Laraib Tabbassum Full Stack Developer'],
                                 autoStart: true,
@@ -21,25 +20,20 @@ export default function Hero() {
                             }}
                         />
                     </div>
-                    <p className="text-left w-96 m-10 mb-48">
-                        I am a dedicated Full Stack Web Developer with a strong focus on building 
-                        responsive, dynamic web applications. My expertise lies in HTML, CSS, Tailwind CSS,
-                        JavaScript, and TypeScript, which enables me to create intuitive and efficient user experiences.
-                        With a commitment to writing clean, maintainable code, I continuously strive to
-                        deliver high-quality solutions. Currently, I am expanding my skills by learning Next.js, which allows me to develop scalable,
-                        high-performance web applications that meet modern industry standards.
+                    <p className="text-left w-full md:w-96 mb-8 sm:mb-12">
+                        "I am a Full Stack Web Developer skilled in HTML, CSS, Tailwind CSS, JavaScript, and TypeScript, focused on building responsive, dynamic applications. Currently, I’m expanding my expertise with Next.js to create scalable, high-performance solutions."
                     </p>
-                    <a href="cv.pdf" className="flex justify-self-center px-40 py-0">
-                        <button className="inline-flex rounded-full text-white font-semibold bg-orange-600 border-0 py-2 px-6 hover:bg-orange-400 text-lg border-white">
+                    <a href="cv.pdf" className="flex justify-self-center px-0 md:px-40 py-0">
+                        <button className="inline-flex rounded-full text-white font-semibold bg-orange-600 border-0 py-2 px-6 hover:bg-orange-400 text-lg">
                             Resume
                         </button>
                     </a>
                 </div>
                 
                 {/* Image Section */}
-                <div className="flex-1 flex justify-end md:w-1/2 mr-16">
-                    <div className="rounded-lg shadow-lg mb-16">
-                        <img src="\pic.jpg" height="100" width="250" alt="hero" />
+                <div className="flex-1 flex justify-center md:justify-end md:w-1/2">
+                    <div className="rounded-lg shadow-lg mb-20 sm:mb-0 sm:mt-10">
+                        <Image src="/pic.jpg" height={100} width={150} alt="hero" className="object-cover" />
                     </div>
                 </div>
             </div>
